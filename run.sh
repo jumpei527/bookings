105e5276f2869633bb0634ae3b8420ea5d44c249
jumpei527 <jumpei5278@gmail.com>
2024-05-19 16:40:06 +0900

    modify database.yml and run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ read -sp "Enter Database Password: " DBPASS
 export DBPASS
 
 go build -o bookings cmd/web/*.go
-./bookings -dbname=bookings -dbuser=postgres -cache=false -production=false -dbpass=$DBPASS
+./bookings -cache=false -production=false -dbpass=$DBPASS
